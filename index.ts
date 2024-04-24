f28b850309a36d3afef935080f6d03630e505cc2
unknown <syedriazali1997@gmail.com>
2024-04-25 00:39:25 +0500

    First push

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer"
+
+// Step - 1 : Computer will generate a random number
+
+const randomNumber = Math.floor(Math.random()* 10 + 1);
+
+// Step - 2 : User Input for guessing number
+
+const answer = await inquirer.prompt([
+    {
+        name : "userGuessedNumber",
+        type : "number",
+        message : "Please guess a number b/w range 1 - 10 : "
+    }
+]);
+
+console.log(answer)
+
+// Step - 3 : Compare user input number with computer generated number and show results
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed the correct number");
+} else {
+    console.log("You guessed the wrong number")
+}
